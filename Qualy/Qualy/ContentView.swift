813b21c6fda66c1ba5610f5bcbf7509a312fca44
Daniel <d.krivelev@redmadrobot.com>
2022-04-20 21:33:20 +0700

    init

---
+++ Qualy/Qualy/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  Qualy
+//
+//  Created by Daniel Krivelev on 20.04.2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+        .font(.custom("Manrope-Bold", size: 24))
+            .padding()
+            
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Qualy/Qualy/QualyApp.swift
@@ -0,0 +1,17 @@
+//
+//  QualyApp.swift
+//  Qualy
+//
+//  Created by Daniel Krivelev on 20.04.2022.
+//
+
+import SwiftUI
+
+@main
+struct QualyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
